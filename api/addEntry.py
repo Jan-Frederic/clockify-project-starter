d5fdd85256f734aeae8de06b0f0174f242b856ad
Jan-Frederic Overbeck <jan-frederic.overbeck@flender.com>
2023-03-13 09:09:21 +0100

    add batch files

---
+++ api/addEntry.py
@@ -0,0 +1,21 @@
+import requests
+import datetime
+import sys
+from datetime import datetime
+
+api_key= sys.argv[1]
+user_id= sys.argv[2]
+workspace_id= sys.argv[3]
+description = sys.argv[4]
+project_id = sys.argv[5]
+
+data = {
+    "billable": "true",
+    "description": description,
+    "projectId": project_id
+}
+
+url = f"https://api.clockify.me/api/v1/workspaces/{workspace_id}/user/{user_id}/time-entries"
+
+r = requests.post(url, json=data, headers={"X-Api-Key": api_key})
+print(r.content)    
